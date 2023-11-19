e1bf2e229362814c43bea19c9083936d1c4e8391
Matias Nahuel Castro Russo <matcastr@c2r13s5.42madrid.com>
2023-11-19 11:06:08 +0100

    perm

--- rush01/ex00/perm.c
+++ rush01/ex00/perm.c
@@ -6,64 +6,79 @@
 /*   By: hmalzac <hmalzac@student.42madrid.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 20:00:06 by hmalzac           #+#    #+#             */
-/*   Updated: 2023/11/18 20:57:24 by hmalzac          ###   ########.fr       */
+/*   Updated: 2023/11/19 11:03:00 by hmalzac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void	ft_print_comb(char tab[24][5])
+void	ft_store_comb(char *digits, char tab[24][5], int *index)
 {
 	char	a;
 	char	b;
 	char	c;
 	char	d;
-	int	i = 0;
 
-	a = '1';
-	while (a <= '4')
+	a = digits[0];
+	b = digits[1];
+	c = digits[2];
+	d = digits[3];
+	if (a != b && a != c && a != d && b != c && b != d && c != d)
 	{
-		b = '1';
-		while (b <= '4')
-		{
-			c = '1';
-			while (c <= '4')
-			{
-				d = '1';
-				while (d <= '4')
-				{
-					if(a != b && a != c && a != d && b != c && b != d && c != d)
-					{
-					
-					tab[i][0] = a;
-                    tab[i][1] = b;
-                    tab[i][2] = c;
-                    tab[i][3] = d;
-                    tab[i][4] = '\0';
-					i++;
-					}
-					d++;
-				}
-				c++;
-			}
-			b++;
-		}
-		a++;
+		tab[*index][0] = a;
+		tab[*index][1] = b;
+		tab[*index][2] = c;
+		tab[*index][3] = d;
+		tab[*index][4] = '\0';
+		(*index)++;
 	}
 }
 
-int main()
-{
-	char tab[24][5];
-	int j = 0;
 
-    ft_print_comb(tab);
-	
-	while(j < 24)
+void	ft_print_comb(char tab[24][5])
+{
+	char	*digits;
+	int	i;
+
+	digits = (char *)malloc(5);
+	digits = "1111\0";
+	printf("&p", digits);
+//	digits = {'1', '1', '1', '1', '0'};
+	while (digits[0] <= '4')
+	{
+		digits[1] = '1';
+		while (digits[1] <= '4')
+		{
+			digits[2] = '1';
+			while (digits[2] <= '4')
+			{
+				digits[3] = '1';
+				while (digits[3] <= '4')
+				{
+					ft_store_comb(digits, tab, &i);
+					digits[3]++;
+				}
+				digits[2]++;
+			}
+			digits[1]++;
+		}
+		digits[0]++;
+	}
+}
+
+int	main(void)
+{
+	char	tab[24][5];
+	int	j;
+
+	j = 0;
+	ft_print_comb(tab);
+	while (j < 24)
 	{
 		printf("tab[%d] = %s\n", j, tab[j]);
 		j++;
 	}
-    return 0;
+	return (0);
 }
